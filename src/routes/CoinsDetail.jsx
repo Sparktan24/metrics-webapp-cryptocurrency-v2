8b6725f8aa427db4d6640812155646a68e30a025
Jesus Molina <netod90@gmail.com>
2023-05-29 20:29:56 -0700

    Add and fix linter

--- src/routes/CoinsDetail.jsx
+++ src/routes/CoinsDetail.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const CoinsDetail = () => {
-  return (
-    <div>CoinsDetail</div>
-  )
-}
+const CoinsDetail = () => (
+  <div>CoinsDetail</div>
+);
 
-export default CoinsDetail
+export default CoinsDetail;
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+const HomePage = () => (
+  <div>HomePage</div>
+);
 
-export default HomePage
+export default HomePage;
